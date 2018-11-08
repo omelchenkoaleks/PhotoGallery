e55c3f4d016c5219ff3afc9dcf35eb1d9fd87e1b
omelchenkoaleks <omelchenkoaleks@gmail.com>
2018-11-08 10:18:22 +0200

    Создали внутренний класс AsyncTask (фоновый поток) и проверили в нем сетевой код класса FlickrFetchr.

--- app/src/main/java/com/omelchenkoaleks/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/omelchenkoaleks/photogallery/PhotoGalleryFragment.java
@@ -1,5 +1,6 @@
 package com.omelchenkoaleks.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,11 +12,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
+
 public class PhotoGalleryFragment extends Fragment {
 
-    private final String TAG = this.getClass().getSimpleName();
+    private static final String TAG = "PhotoGalleryFragment";
 
     private RecyclerView photoRecyclerView;
+
+    // Класс помогает сделать тест сетевому классу в фоновом режиме и вывести
+    // в журнал результат запроса по URL.
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            try {
+                String result = new FlickrFetchr().getUrlString("https://www.bignerdranch.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+            }
+            return null;
+        }
+    }
 
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
@@ -28,6 +48,9 @@ public class PhotoGalleryFragment extends Fragment {
 
         // Зачем удерживаем фрагмент?
         setRetainInstance(true);
+        // Запускаем тестовый метод в фоновом режиме с помощью AsyncTask,
+        // чтобы сделать тест классу FlickrFetchr.
+        new FetchItemsTask().execute();
     }
 
     @Nullable
