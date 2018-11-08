ce1ccfe61e69463eb565884e4ead469833c26afb
omelchenkoaleks <omelchenkoaleks@gmail.com>
2018-11-08 11:50:31 +0200

    Создан метод, который строит URL-адрес запроса и загружает все его содержимое. Убран тестовый код в фоновом потоке AsyncTask и заменен на запрос.

--- app/src/main/java/com/omelchenkoaleks/photogallery/FlickrFetchr.java
+++ app/src/main/java/com/omelchenkoaleks/photogallery/FlickrFetchr.java
@@ -1,5 +1,8 @@
 package com.omelchenkoaleks.photogallery;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,6 +12,11 @@ import java.net.URL;
 // Класс, который будет поддерживать все
 // сетевые взаимоотношения в этом приложении.
 public class FlickrFetchr {
+
+    private static final String TAG = "FlickrFetchr";
+
+    // Константа хранит некомерческий ключ для доступа на сайт Flickr.
+    private static final String API_KEY = "fb6c7b37137c4e7ec8309d42ce7aa1a7";
 
     // Метод получает низкоуровневые данные по URL и
     // возвращает их в виде массива байтов.
@@ -49,5 +57,23 @@ public class FlickrFetchr {
     // Метод преобразует полученные данные методом getUrlBytes(String) в String.
     public String getUrlString(String urlSpec) throws IOException {
         return new String(getUrlBytes(urlSpec));
+    }
+
+    // Метод, который строит соответствующий URL-адрес запроса и загружает его содержимое.
+    public void fetchItems() {
+        try {
+            String url = Uri.parse("https://api.flickr.com/services/rest/")
+                    .buildUpon()
+                    .appendQueryParameter("method", "flickr.photos.getRecent")
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter("format", "json")
+                    .appendQueryParameter("nojsoncallback", "1")
+                    .appendQueryParameter("extras", "url_s")
+                    .build().toString();
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Received JSON: " + jsonString);
+        } catch (IOException ioe) {
+            Log.e(TAG, "Failed to fetch items", ioe);
+        }
     }
 }
--- app/src/main/java/com/omelchenkoaleks/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/omelchenkoaleks/photogallery/PhotoGalleryFragment.java
@@ -26,13 +26,8 @@ public class PhotoGalleryFragment extends Fragment {
 
         @Override
         protected Void doInBackground(Void... params) {
-
-            try {
-                String result = new FlickrFetchr().getUrlString("https://www.bignerdranch.com");
-                Log.i(TAG, "Fetched contents of URL: " + result);
-            } catch (IOException ioe) {
-                Log.e(TAG, "Failed to fetch URL: ", ioe);
-            }
+            // Теперь вызываем уже собранный URL-адресс в методе fetchItems().
+            new FlickrFetchr().fetchItems();
             return null;
         }
     }
