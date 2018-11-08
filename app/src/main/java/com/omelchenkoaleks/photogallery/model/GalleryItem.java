3d78e10fe56c3fdc360311a93bebd06f64a6463a
omelchenkoaleks <omelchenkoaleks@gmail.com>
2018-11-08 13:07:22 +0200

    Создан класс модели, в который будем помещать полученные данные в разметке JSON.

---
+++ app/src/main/java/com/omelchenkoaleks/photogallery/model/GalleryItem.java
@@ -0,0 +1,37 @@
+package com.omelchenkoaleks.photogallery.model;
+
+// Класс, поможет помещать данные в объекты модели.
+public class GalleryItem {
+
+    private String caption;
+    private String id;
+    private String url;
+
+    public String toString() {
+        return caption;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
